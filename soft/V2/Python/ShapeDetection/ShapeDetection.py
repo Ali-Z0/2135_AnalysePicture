39b62eb934b02f6852e3d1acbf1cacaf039b6455
Ali-Z0 <ali.zoubir@bluewin.ch>
2023-02-12 10:42:20 +0100

    Changed files to good code

--- soft/V2/Python/ShapeDetection/ShapeDetection.py
+++ soft/V2/Python/ShapeDetection/ShapeDetection.py
@@ -4,90 +4,77 @@ import random
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import argparse
 import os
 
-img_folder=r'C:\Users\alizoubir\Documents\ETML-ES-2eme\POBJ\2135_AnalysePicture\soft\V2\Shapes-Dataset\output'
+# Chemin d'accés au dossier contenant le dataset de formes
+img_folder=r'F:\ETML-ES\POBJ\2135_AnalysePicture\soft\V2\Shapes-Dataset\output'
 
+# Arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-Th','--T_upper', type=float, default=40, help="Valeur seuil supérieur dans le Seuil d'hystérésis")
+parser.add_argument('-Tl','--T_lower', type=float, default=20, help="Valeur seuil inférieur dans le Seuil d'hystérésis")
+parser.add_argument('-n','--nbImg', type=float, default=5, help="Nombre d'images a analyser")
+parser.add_argument('-b','--nbBatch', type=float, default=2, help='Nombre de batch de figures')
+args = parser.parse_args()
 
-#datasetPath = "./Shapes-Dataset/output"
+# ------ Hyperparamètres------
+# Valeur seuil inférieure dans le Seuil d'hystérésis
+T_lower = args.T_lower
+# Valeur seuil supérieur dans le Seuil d'hystérésis
+T_upper = args.T_lower
 
-plt.figure(figsize=(20,20))
+# ------ Parametres interface ------
+# Nombre de batch de figures
+nbBatch = args.nbBatch
+# Nombre d'images a analyser
+nbImg = args.nbImg
+# Noms des formes selon les cotés
+shapes = np.array(["Rien", "Trait", "Angle", "Triangle","Quadrillatere","Pentagone",
+                    "Hexagone", "Heptagone", "Octogone", "Nonagone", "Etoile"])
 
+for b in range(nbBatch):
+    # Dimension des figures affichées
+    plt.figure(figsize=(15,15))
 
-for i in range(5):
-    file = random.choice(os.listdir(img_folder))
-    image_path= os.path.join(img_folder, file)
-    img=mpimg.imread(image_path)
-    ax=plt.subplot(2,5,i+1)
-    ax.title.set_text("Image " + str(i))
-    plt.imshow(img)
+    # Boucle pour analyse du nombre d'images
+    for i in range(nbImg):
+        # Choix de l'image aléatoire
+        file = random.choice(os.listdir(img_folder))
+        # Joins le nom du fichier image avec chemin d'accés du dataset
+        image_path= os.path.join(img_folder, file)
+        # Charge l'image dans une variable
+        img=mpimg.imread(image_path)
+        # Prépare l'emplacement de l'image dans la figure sur 2 colonnes
+        ax=plt.subplot(2,nbImg,i+1)
+        ax.title.set_text("Image " + str(i+1))
+        # Ajoute l'image à la figure
+        plt.imshow(img)
 
+        # Charge la meme image dans une variable mais en 2 couleurs
+        grayscale = cv2.imread(image_path,0)
+        # Utilisation d'une fonction pour détécter les bords de l'images
+        edged = cv2.Canny(grayscale, T_lower, T_upper)
+        # Trouve le nombre de contours et les charges dans une variable contenant leurs caractéristiques
+        contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        # Selectionne l'un des contours 
+        cnt = contours[0]
+        # Fait l'approximation de la forme du contour séléctionné
+        approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
 
-    # Convert image in grayscale
-    grayscale = cv2.imread(image_path,0)
-    #grayscale = cv2.cvtColor(np.float32(img), cv2.COLOR_RGB2GRAY)
-    
-    # setting threshold of gray image
-    #_, threshold = cv2.threshold(grayscale, 127, 255, 0)
-    # using a findContours() function
-    #contour, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    #edge = cv2.Canny((grayscale*255).astype(np.uint8), 10, 20)
-
-    edged = cv2.Canny(grayscale, 30, 200)
-
-    #contours = cv2.findContours(edge.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    #contours, hierarchy = cv2.findContours(threshold, 1, 2) 
-
-    contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-    ax=plt.subplot(2,5,i+1+5)
-    ax.title.set_text("Image Shape " + str(i))
-
-    # list for storing names of shapes
-    for contour in contours:
-      
-        # here we are ignoring first counter because 
-        # findcontour function detects whole image as shape
-        if i == 0:
-            i = 1
-            continue
-      
-        # cv2.approxPloyDP() function to approximate the shape
-        approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
-
-        # using drawContours() function
-        plt.imshow(cv2.drawContours(grayscale, [contour], 0, (0, 0, 255), 2))
-      
-        # finding center point of shape
-        M = cv2.moments(contour)
-        if M['m00'] != 0.0:
-            x = int(M['m10']/M['m00'])
-            y = int(M['m01']/M['m00'])
-      
-        # putting shape name at center of each shape
-        if len(approx) == 3:
-            print("Triangle")
-      
-        elif len(approx) == 4:
-            print("Quadrilatere")
-      
-        elif len(approx) == 5:
-            print("Pentagone")
-      
-        elif len(approx) == 6:
-            print("Hexagone")
-      
+        # Détérmine le nom de la forme selon le nombre de cotés de la forme approximée
+        if(len(approx) < 11):
+            shape = shapes[len(approx)]
         else:
-            print("Cercle")
+            shape = "Cercle"
 
-plt.show()
+        # Prépare l'emplacement de l'image dans la figure sur la 2ème colonnes
+        ax=plt.subplot(2,nbImg,i+1+nbImg)
+        ax.title.set_text(shape)
+        # Ajoute l'image a la figure
+        plt.imshow(cv2.drawContours(grayscale, [cnt], 0, (0, 0, 255), 2))
 
-# convert to grayscale
-
-#grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# perform edge detection
-#edges = cv2.Canny(grayscale, 30, 100)
-
-#ax=plt.subplot(1,6,6)
-#ax.title.set_text("Image 6")
+    # Sauvegarde la figure
+    plt.savefig("ShapesBatchN"+str(b+1))
+    # Affiche la figure du batch
+    plt.show()
--- soft/V2/Python/ShapeDetection/Tests.py
+++ soft/V2/Python/ShapeDetection/Tests.py
@@ -25,7 +25,7 @@ for i in range(5):
 
     # Convert image in grayscale
     grayscale = cv2.imread(image_path,0)
-    #grayscale = cv2.cvtColor(np.float32(img), cv2.COLOR_BGR2GRAY)
+    #grayscale = cv2.cvtColor(np.float32(img), cv2.COLOR_RGB2GRAY)
     
     # setting threshold of gray image
     #_, threshold = cv2.threshold(grayscale, 127, 255, 0)
@@ -33,40 +33,52 @@ for i in range(5):
     #contour, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     #edge = cv2.Canny((grayscale*255).astype(np.uint8), 10, 20)
 
-    edged = cv2.Canny(grayscale, 15, 30)
+    edged = cv2.Canny(grayscale, 30, 200)
 
     #contours = cv2.findContours(edge.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     #contours, hierarchy = cv2.findContours(threshold, 1, 2) 
 
     contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    cnt = contours[0]
-
-    # cv2.approxPloyDP() function to approximate the shape
-    approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
-
-    if(len(approx) == 3):
-        shape = "Triangle"
-    elif(len(approx) == 4):
-        shape = "Quadrillatere"
-    elif(len(approx) == 5):
-        shape = "Pentagone"
-    elif(len(approx) == 6):
-        shape = "Hexagone"
-    elif(len(approx) == 7):
-        shape = "Heptagone"
-    elif(len(approx) == 8):
-        shape = "Octogone"
-    elif(len(approx) == 9):
-        shape = "Nonagon"
-    else:
-        shape = "Cercle"
 
     ax=plt.subplot(2,5,i+1+5)
-    ax.title.set_text(shape)
-    # using drawContours() function
-    plt.imshow(cv2.drawContours(grayscale, [cnt], 0, (0, 0, 255), 2))
+    ax.title.set_text("Image Shape " + str(i))
 
+    # list for storing names of shapes
+    for contour in contours:
+      
+        # here we are ignoring first counter because 
+        # findcontour function detects whole image as shape
+        if i == 0:
+            i = 1
+            continue
+      
+        # cv2.approxPloyDP() function to approximate the shape
+        approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
 
+        # using drawContours() function
+        plt.imshow(cv2.drawContours(grayscale, [contour], 0, (0, 0, 255), 2))
+      
+        # finding center point of shape
+        M = cv2.moments(contour)
+        if M['m00'] != 0.0:
+            x = int(M['m10']/M['m00'])
+            y = int(M['m01']/M['m00'])
+      
+        # putting shape name at center of each shape
+        if len(approx) == 3:
+            print("Triangle")
+      
+        elif len(approx) == 4:
+            print("Quadrilatere")
+      
+        elif len(approx) == 5:
+            print("Pentagone")
+      
+        elif len(approx) == 6:
+            print("Hexagone")
+      
+        else:
+            print("Cercle")
 
 plt.show()
 
